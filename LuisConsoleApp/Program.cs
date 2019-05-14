20e2154198f0be4eb4af04dd87850811f4a79c46
Rodrigo Riccitelli Vieira <mcbacker@gmail.com>
2019-05-14 18:38:37 -0300

    Luis Sample

---
+++ LuisConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
+using System;
+using System.Configuration;
+
+namespace LuisConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LUISRuntimeClient luisClient = new LUISRuntimeClient(
+                new ApiKeyServiceClientCredentials(ConfigurationManager.AppSettings["LUIS.SubscriptionKey"]))
+            { Endpoint = "https://westus.api.cognitive.microsoft.com/" };
+
+            while (true)
+            {
+                Console.Write("Input: ");
+
+                LuisResult luisResult = luisClient.Prediction.ResolveAsync(ConfigurationManager.AppSettings["LUIS.ApplicationId"], Console.ReadLine()).Result;
+
+                if (luisResult.TopScoringIntent != null)
+                {
+                    Console.WriteLine($"TopScoringIntent: {luisResult.TopScoringIntent.Intent} - Score: {luisResult.TopScoringIntent.Score}");
+                }
+
+                if (luisResult.Intents != null)
+                {
+                    foreach (IntentModel luisIntent in luisResult.Intents)
+                    {
+                        Console.WriteLine($"TopScoringIntent: {luisIntent.Intent} - Score: {luisIntent.Score}");
+                    }
+                }
+            }
+        }
+    }
+}
